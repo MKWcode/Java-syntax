798e688d18734f79153444a7dd520ec754ed439d
MKWcode <michelle.woo588@gmail.com>
2025-05-31 12:03:18 -0400

    updated File_writer.java

--- File_writer.java
+++ File_writer.java
@@ -13,9 +13,22 @@ public class File_writer {
       		e.printStackTrace();
     		}
     	}
+	public static void write_with_numbers(String file_Name){
+	try {
+      		FileWriter writer = new FileWriter(file_Name);
+		int num = -5;
+      		writer.write("Writing the number "+num);
+     		writer.close();
+      		System.out.println("Successfully wrote to the file.");
+    	} catch (IOException e) {
+      		System.out.println("An error occurred.");
+      		e.printStackTrace();
+    		}
+    	}
  	
 
   public static void main(String[] args) {
     	write("Messages.txt");
+	write_with_numbers("Numbers");
   }
 }
